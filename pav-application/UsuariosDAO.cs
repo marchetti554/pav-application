5be993927fdfea66876fbd4649736cfb18fe4423
Marco Marchetti <marco-marchetti@hotmail.com>
2019-09-03 19:16:50 -0300

    Minor changes.

---
+++ pav-application/UsuariosDAO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pav_application
+{ 
+    class UsuariosDAO
+    {
+        SqlConnection cnn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        DataTable tabla = new DataTable();
+
+        private void Si()
+        {
+            cnn.ConnectionString = "Data Source=DESKTOP-APONHGI;Initial Catalog=pav-db;Integrated Security=True";
+            cnn.Open();
+        }
+    }
+
+    class Usuario
+    {
+        String nombre_usuario;
+        String password;
+    }
+}
